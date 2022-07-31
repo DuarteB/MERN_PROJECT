81474b4ec2e127604a89d2925d5ba7d2a1acbe96
DuarteB <eduarte.dev@gmail.com>
2022-07-31 20:34:06 -0300

    Creating front-end structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { Layout, Image, Typography } from "antd";
+import Logo from "./images/camera.png";
+import Home from "./components/Home";
+
+const { Title } = Typography;
+const { Header, Footer } = Layout;
 
 const App = () => {
-    return(<>
-        <p>
-            "Rendering test";
-        </p>
-    </>);
+    return(
+        <Layout>
+                <Header>
+                    <Image width="45" preview={false} src={Logo}/>
+                    &nbsp;
+                    <Title>Instaclone</Title>
+                </Header>
+                <Home />
+                <Footer>
+                    2022 Instaclone
+                </Footer>
+        </Layout>
+    );
 }
 
 export default App;
